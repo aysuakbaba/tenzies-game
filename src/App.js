9b4deda4a547d2dbf4a09d421ca42f4209823365
aysuakbaba <aysuakbabaa@gmail.com>
2023-01-26 15:47:33 +0300

    feat(App component): add allNewDices function

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import Die from "./components/Die"
 
 
 function App() {
+  const [dice , setDice] = React.useState(allNewDices)
+
+    function allNewDices(){
+        const newDice = []
+        for(let i=0; i<10; i++){
+            newDice.push(Math.ceil(Math.random()*6))
+        }
+        return newDice
+    }
+
+    const diceElement = dice.map(die => <Die value={die}/>)
   return (
       <main>
         <div className='container'>
-          <Die value="1" />
-          <Die value="2" />
-          <Die value="3" />
-          <Die value="4" />
-          <Die value="5" />
-          <Die value="6" />
-          <Die value="7" />
-          <Die value="8" />
-          <Die value="9" />
-          <Die value="10" />
+        {diceElement}
         </div>
         
       
--- src/components/Die.js
+++ src/components/Die.js
@@ -3,18 +3,13 @@ import React from "react";
 
 
 function Die(props) {
+    
 
-    function allNewDices(){
-        const newDice = []
-        for(let i=0; i<10; i++){
-            newDice.push(Math.ceil(Math.random()*6))
-        }
-        return newDice
 
-    }
     return(
         <div className="die-face">
             <h2 className="die-num">{props.value}</h2>
+           
 
         </div>
     )
