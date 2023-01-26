09460b1cb2dd198b999b84857090958feca1d974
aysuakbaba <aysuakbabaa@gmail.com>
2023-01-26 13:59:16 +0300

    refactor(styling): styling of Die component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import './App.css';
+import './index.css';
+import Die from "./components/Die"
+
 
 function App() {
   return (
-    <div className="App">
+      <main>
+        <div className='container'>
+          <Die value="1" />
+          <Die value="2" />
+          <Die value="3" />
+          <Die value="4" />
+          <Die value="5" />
+          <Die value="6" />
+          <Die value="7" />
+          <Die value="8" />
+          <Die value="9" />
+          <Die value="10" />
+        </div>
+        
       
-    </div>
+
+      </main>
+  
   );
 }
 
---
+++ src/components/Die.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Die(props) {
+    return(
+        <div className="die-face">
+            <h2 className="die-num">{props.value}</h2>
+
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Die;
