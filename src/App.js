89ae2d2265033c08ac731eb0dbfab269f2f2bc88
aysuakbaba <aysuakbabaa@gmail.com>
2023-01-29 17:33:29 +0300

    feat(Die component): add color property to isHeld

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import './index.css';
-import Die from "./components/Die"
-
+import React from "react";
+import "./index.css";
+import Die from "./components/Die";
 
 function App() {
-  const [dice , setDice] = React.useState(allNewDices)
+  const [dice, setDice] = React.useState(allNewDices());
 
-    function allNewDices(){
-        const newDice = []
-        for(let i=0; i<10; i++){
-            newDice.push(Math.ceil(Math.random()*6))
-        }
-        return newDice
+  function allNewDices() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: true,
+      });
+     
     }
+    return newDice;
+  }
 
-    function rollDice(){
-      setDice(allNewDices)
-    }
+  function rollDice() {
+    setDice(allNewDices());
+  }
 
-    const diceElement = dice.map(die => <Die value={die}/>)
+  const diceElement = dice.map((die) => <Die value={die.value} isHeld={die.isHeld} />);
   return (
-      <main>
-        <div className='container'>
-        {diceElement}
-        </div>
-        <button onClick={rollDice} className="roll-dice">Roll</button>
-        
-      
-
-      </main>
-  
+    <main>
+      <div className="container">{diceElement}</div>
+      <button onClick={rollDice} className="roll-dice">
+        Roll
+      </button>
+    </main>
   );
 }
 
--- src/components/Die.js
+++ src/components/Die.js
@@ -3,11 +3,13 @@ import React from "react";
 
 
 function Die(props) {
+
+    const dieStyle={
+        backgroundColor: props.isHeld ? "#03C988" : "white" 
+    }
     
-
-
     return(
-        <div className="die-face">
+        <div className="die-face" style={dieStyle}>
             <h2 className="die-num">{props.value}</h2>
            
 
